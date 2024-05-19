1c949705d33d500bec9564343c448f7040e5a36d
Thayna Mac Dolw <thaymacdolw@gmail.com>
2024-05-19 18:52:10 +0200

    Adding Css file and creating HTML structure

--- first-project-react/src/App.js
+++ first-project-react/src/App.js
@@ -1,5 +1,20 @@
-import React from 'react'
+import React from 'react';
+// JSX é a misturar HTML com JS dentro do React. Para acrescentar o código JS dentro ali da função React,preciso colocar {}.
+import { Container, Image, ContainerItens, H1, InputLabel, Input, Button } from './styles';
+
 const App = () => {
-  return <div>Hello World</div>
+  return (
+    <Container>
+      <Image />
+      <ContainerItens>
+        <H1>Hello!</H1>
+        <InputLabel>Name</InputLabel>
+        <Input placeholder='Name'/>
+        <InputLabel>Age</InputLabel>
+        <Input placeholder='Age'/>
+        <Button>Register</Button>
+      </ContainerItens>
+    </Container>
+  );
 }
 export default App
