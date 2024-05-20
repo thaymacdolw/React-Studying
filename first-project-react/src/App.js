b6aa1d133bdfde6439dc5d025b73a09d680a529c
Thayna Mac Dolw <thaymacdolw@gmail.com>
2024-05-20 16:24:29 +0200

    finishing some styling + getting info from input + deleting users function

--- first-project-react/src/App.js
+++ first-project-react/src/App.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 // JSX é a misturar HTML com JS dentro do React. Para acrescentar o código JS dentro ali da função React,preciso colocar {}.
-import { Container, Image, ContainerItens, H1, InputLabel, Input, Button } from './styles';
+import People from './assets/people.svg';
+import Arrow from './assets/arrow.svg';
+import Trash from './assets/trash.svg';
+
+import { Container, Image, ContainerItens, H1, InputLabel, Input, Button, User } from './styles';
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+  const inputName = useRef()
+  const inputAge = useRef()
+
+  function addNewUser() {
+    setUsers([
+      ...users, 
+      { 
+        id: Math.random(), 
+        name: inputName.current.value,
+        age: inputAge.current. value 
+      },
+    ]);
+  }
+  function deleteUser(userId) {
+    const newUsers = users.filter (user => user.id !== userId)
+    setUsers(newUsers);
+  }
   return (
     <Container>
-      <Image />
+      <Image alt="logo-imagem" src={People} />
       <ContainerItens>
         <H1>Hello!</H1>
         <InputLabel>Name</InputLabel>
-        <Input placeholder='Name'/>
+        <Input ref={inputName} placeholder='Name' />
         <InputLabel>Age</InputLabel>
-        <Input placeholder='Age'/>
-        <Button>Register</Button>
+        <Input ref={inputAge} placeholder='Age' />
+        <Button onClick={addNewUser}>Register <img alt="arrow" src={Arrow} /></Button>
+        <ul>
+          {users.map((user) => (
+            <User key={user.id}>
+              <p>{user.name}</p> <p>{user.age}</p>
+              <button onClick={() => deleteUser(user.id)}><img src={Trash} alt="trash can" /></button>
+            </User>
+          ))}
+        </ul>
       </ContainerItens>
     </Container>
   );
--- first-project-react/src/styles.js
+++ first-project-react/src/styles.js
@@ -1,22 +1,120 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Background from './assets/background1.svg';
 
 export const Container = styled.div`
-
+    background: url("${Background}");
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 40px;
 `;
 export const Image = styled.img`
-
+    margin-top:30px;
+    
 `;
 export const ContainerItens = styled.div`
-
+    background: linear-gradient(
+        157.44deg,
+        rgba(255, 255, 255, 0.6) 0.84%,
+        rgba(255, 255, 255, 0.6) 0.85%,
+        rgba(255, 255, 255, 0.15) 100%
+    );
+    border-radius: 61px 61px 0px 0px;
+    padding: 50px 36px;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
 `;
+
 export const H1 = styled.h1`
- 
+    font-style: normal;
+    font-size: 34px;
+    font-weight: bold;
+    line-height: 40px;
+    text-align: center;
+    color: #FFFFFF;
+    margin-bottom:80px;
+
 `
-export const InputLabel = styled.p `
+export const InputLabel = styled.p`
+    letter-spacing: -0.408px;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 22px;
+    color:#FFFFFF;
+    margin-left:25px;
+
 `
 
-export const Input = styled.input `
+export const Input = styled.input`
+    background: rgba( 255, 255, 255, 0.25);
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    border-radius:14px;
+    width: 342px;
+    height: 58px;
+    padding-left: 25px;
+    margin-bottom: 34px;
+    border: none;
+    outline: none;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 20px;
+    line-height: 28px;
+    color:#FFFFFF;
 `
-export const Button = styled.button `
-`
+export const Button = styled.button`
+    width: 342px;
+    height: 74px;
+    background: rgba(0, 0, 0, 0.8);
+    border-radius: 14px;    
+    border:none;
+    font-style:normal;
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 28px;
+    color: #FFFFFF;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 18px;
+
+    &:hover {
+        opacity: 0.8;
+    }
+    &:active {
+        opacity: 0.5;
+    }
+`;
+export const User = styled.li `
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin-top: 20px;
+    background: rgba( 255, 255, 255, 0.25);
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    border-radius:14px;
+    width: 342px;
+    height: 58px;
+    padding-left: 25px;
+    margin-bottom: 34px;
+    border: none;
+    outline: none;
+
+    p {
+        font-style:normal;
+        font-weight:normal;
+        font-size:20px;
+        line-height: 28px;
+        color: #FFFFFF;
+    }
+
+    button {
+        background: none;
+        border: none;
+        cursor: pointer;
+    }
+`;
 
